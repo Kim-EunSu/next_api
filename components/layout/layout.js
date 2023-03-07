237630db7837fc2c785f7c101752dc1c21003659
Kim-EunSu <kes73640@gmail.com>
2023-03-07 14:20:03 +0900

    createContext 추가

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,13 +1,26 @@
-import { Fragment } from 'react';
-
-import MainHeader from './main-header';
+import { useContext } from "react";
+import MainHeader from "./main-header";
+import Notification from "../components/ui/notification";
+import NotificationContext from "../../store/notification-context";
+import { useContext } from "react";
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
-    <Fragment>
+    <>
       <MainHeader />
       <main>{props.children}</main>
-    </Fragment>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
+    </>
   );
 }
 
