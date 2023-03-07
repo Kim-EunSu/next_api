aa450d2dd077fc4ff5cce246b1232803ae4df3bc
Kim-EunSu <kes73640@gmail.com>
2023-03-07 14:11:57 +0900

    createContext 추가

---
+++ store/notification-context.js
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+
+const NotificationContext = createContext({
+  notification: null, //{title, message, status}
+  showNotification: function (notificationData) {},
+  hideNotification: function () {},
+});
+
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  //함수들을 하나의 객체로 모아 앱의 다른 컴포넌트에 context로 배포
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
+
+export default NotificationContext;
