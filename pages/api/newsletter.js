64714ca23fc22c0c85258d00feeda05de5a26b94
Kim-EunSu <kes73640@gmail.com>
2023-03-07 12:11:23 +0900

    데이터베이스 POST & GET

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -12,9 +12,9 @@ async function handler(req, res) {
     const client = await MongoClient.connect(
       "mongodb+srv://EunSu:zcydiDJibDtxOpkn@nextapi.ffspq4w.mongodb.net/?retryWrites=true&w=majority"
     );
-    const db = client.db("newsletter");
+    const db = client.db("events");
 
-    await db.collection("emails").insertOne({ email: userEmail });
+    await db.collection("newsletter").insertOne({ email: userEmail });
 
     client.close();
 
