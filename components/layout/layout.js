2ea8ac9483a0a1261c26047adb420e701998eaf7
Kim-EunSu <kes73640@gmail.com>
2023-03-08 12:08:55 +0900

    status알림 추가

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,8 +1,7 @@
 import { useContext } from "react";
 import MainHeader from "./main-header";
-import Notification from "../components/ui/notification";
+import Notification from "../ui/notification";
 import NotificationContext from "../../store/notification-context";
-import { useContext } from "react";
 
 function Layout(props) {
   const notificationCtx = useContext(NotificationContext);
